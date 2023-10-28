f65a4236c29529c5ef2f2d7b927e47531866f3a9
Peterdinis <peterdinis0@pobox.sk>
2023-10-28 10:05:56 +0200

    feat: Update account urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("login/", views.loginView, name='login'),
-    path("register/", views.registerView, name='register')
+    path("register/", views.registerView, name='register'),
+    path("logout/", views.logoutUser, name="logoutUser")
 ]
